8e8356edb9397119929dc3e8df40a62f7b3bd2f3
ancebuc <ancebuc@unirioja.es>
2022-02-25 22:19:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio4/principal.kt
@@ -0,0 +1,17 @@
+package com.example.ejercicio4
+
+class Principal {
+    fun main() {
+        val myFirstDice = Dice(6)
+        println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
+
+        val mySecondDice = Dice(20)
+        println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+    }
+
+    class Dice(val numSides: Int) {
+        fun roll(): Int{
+            return (1..numSides).random()
+        }
+    }
+}
